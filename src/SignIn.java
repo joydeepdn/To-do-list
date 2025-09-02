7bfbd4022e12d2eee697465c8d542c31d955481e
joydeepdn <deepj0147@gmail.com>
2025-09-02 23:53:14 +0530

    added signup option in signin class

--- src/SignIn.java
+++ src/SignIn.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class SignIn {
@@ -7,23 +8,32 @@ public class SignIn {
     String username=null;
     String password=null;
 
-    public void signIn(Scanner scan,SignUp su) {
 
+    public void signIn(Scanner scan,SignUp su) {
+        String input = null;
         HashMap<String, String> map2 = su.getUser();
         System.out.println(map2);
 
         while (true) {
-            System.out.println();
+            /*System.out.println();
             System.out.print("Enter username:");
             username = scan.nextLine();
             System.out.print("Enter password:");
-            password = scan.nextLine();
-            if(map2.containsKey(this.username) && map2.containsValue(this.password)){
-                to.mainScreen(scan);
+            password = scan.nextLine();*/
+            System.out.println("1.Enter username and password\n2.or SignUp");
+            input = scan.nextLine();
+            if(Objects.equals(input, "1")){
+                System.out.print("Enter username:");
+                username = scan.nextLine();
+                System.out.print("Enter password:");
+                password = scan.nextLine();
+                if(map2.containsKey(this.username) && map2.containsValue(this.password)){
+                    to.mainScreen(scan);
+                }
                 break;
             }
-            else {
-                System.out.println("Incorrect username or password!");
+            else if (Objects.equals(input,"2")){
+
             }
         }
     }
